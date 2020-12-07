d6cdfdf1580b9cc6248c71d18b7d9de7be29a308
David Abram <david@abram.com>
2020-12-07 12:59:05 +0100

    added navigate to /

--- packages/company/website/src/components/Layout/index.jsx
+++ packages/company/website/src/components/Layout/index.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 import "./index.css";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 import "@crocoder-dev/components/lib/main.css";
 import { Navigation, Button } from "@crocoder-dev/components";
 import Footer from "../Footer";
 import CrocNav from "../../images/croc-nav.svg";
 import Head from "../Head";
+import { Location } from '@reach/router';
 
 const Layout = ({ children, stickyFooter, pageTitle, scrollToContactUs }) => {
   return (
-    <>
-      <Head pageTitle={pageTitle} />
-      <Navigation
-        Logo={
-          <Link to="/">
-            <CrocNav />
-          </Link>
-        }
-      >
-        {(toggle) => (
-          <Button
-            onClick={() => {
-              toggle();
-              window.sa_event(
-                `${process.env.GATSBY_SCHEDULE_CALL_NAVIGATION_CLICK_SA_EVENT}`
-              );
-              scrollToContactUs();
-            }}
-            variant="secondary"
+    <Location>
+      {({ location }) => (
+        <>
+          <Head pageTitle={pageTitle} />
+          <Navigation
+            Logo={
+              <Link to="/">
+                <CrocNav />
+              </Link>
+            }
           >
-            Contact Us
-          </Button>
-        )}
-      </Navigation>
-      {children}
-      <Footer sticky={stickyFooter} />
-    </>
+            {(toggle) => (
+              <Button
+                onClick={() => {
+                  toggle();
+                  window.sa_event(
+                    `${process.env.GATSBY_SCHEDULE_CALL_NAVIGATION_CLICK_SA_EVENT}`
+                  );
+                  if(location.pathname !== '/') {
+                    navigate('/');
+                  } else {
+                    scrollToContactUs();
+                  }
+                }}
+                variant="secondary"
+              >
+                Contact Us
+              </Button>
+            )}
+          </Navigation>
+          {children}
+          <Footer sticky={stickyFooter} />
+        </>
+      )}
+    </Location>
   );
 };
 
