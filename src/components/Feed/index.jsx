7a90e7b042337dc7d598475859433d55b14fd322
stefanskoricdev <stephanskoric@gmail.com>
2022-09-09 15:16:06 +0200

    feat: add WebsiteFeed component and rss and json feed page

---
+++ src/components/Feed/index.jsx
@@ -0,0 +1,75 @@
+import { Feed } from "feed";
+import siteConfig from "../../content/site-config.json";
+import authors from "../../content/authors/authors.json";
+import ReactDOMServer from "react-dom/server";
+
+const { siteTitle, siteDescription, siteUrl } = siteConfig;
+
+const feed = new Feed({
+  title: siteTitle,
+  description: siteDescription,
+  id: siteUrl,
+  link: siteUrl,
+  language: "en",
+  image: "", //TODO: Image reference?
+  favicon: "", //TODO: Favicon reference?
+  copyright: "Copyright © CroCoder, Inc. All rights reserved",
+  generator: "awesome", // optional, default = 'Feed for Node.js' => //TODO: Not sure
+  feedLinks: {
+    json: `${siteUrl}/json`,
+  },
+  author: {
+    name: "David Abram",
+    email: "david@crocoder.dev",
+    link: "https://www.linkedin.com/in/david-abram/",
+  },
+});
+
+const WebsiteFeed = ({ pages, type = "rss" }) => {
+  const blogKeys = Object.keys(pages).filter(
+    (key) => key.includes("blog") && key !== "/blog/"
+  );
+  const blogs = blogKeys.map((key) => pages[key]);
+
+  blogs.forEach((blog) => {
+    const blogContent = ReactDOMServer.renderToString(blog.Component());
+
+    const blogAuthor = authors.authors.find(
+      (author) => author.id === blog?.meta?.author
+    );
+
+    const blogContributor = authors.authors.find(
+      (author) => author.id === blog?.meta?.editor
+    );
+
+    feed.addItem({
+      title: blog?.meta?.title,
+      id: blog?.meta?.id,
+      link: `${siteUrl}${blog.urlPath}`,
+      description: blog?.meta?.description,
+      content: blogContent,
+      author: [
+        {
+          name: blogAuthor?.name,
+          email: blogAuthor?.email,
+          link: blogAuthor?.linkedin,
+        },
+      ],
+      contributor: [
+        {
+          name: blogContributor?.name,
+          email: blogContributor?.email,
+          link: blogContributor?.linkedin,
+        },
+      ],
+      image: "", //TODO: Image reference?
+    });
+  });
+
+  const rssEl = feed.rss2();
+  const jsonEl = feed.json1();
+
+  return type === "rss" ? rssEl : type === "json" ? jsonEl : null;
+};
+
+export default WebsiteFeed;
