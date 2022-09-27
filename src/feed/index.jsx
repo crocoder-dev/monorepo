c50932444ae6c57e32f432335bc3e490a30421b0
stefanskoricdev <stephanskoric@gmail.com>
2022-09-27 15:42:27 +0200

    chore: update rss feed

--- src/feed/index.jsx
+++ src/feed/index.jsx
@@ -3,15 +3,15 @@ import siteConfig from "../content/site-config.json";
 import authors from "../content/authors/authors.json";
 import ReactDOMServer from "react-dom/server";
 
-const { siteTitle, siteDescription, siteUrl } = siteConfig;
+const { siteUrl } = siteConfig;
 
 const createFeed = (pages, type = "rss") => {
-  
   const feed = new Feed({
-    title: siteTitle,
-    description: siteDescription,
-    id: siteUrl,
-    link: siteUrl,
+    title: "CroCoder | Blog",
+    description:
+      "Tips and ideas to help you learn, build and improve your projects.",
+    id: `${siteUrl}/blog`,
+    link: `${siteUrl}/blog`,
     language: "en",
     image: "", //TODO: Image reference?
     favicon: "", //TODO: Favicon reference?
@@ -19,7 +19,7 @@ const createFeed = (pages, type = "rss") => {
     generator: "CroCoder feed",
     feedLinks: {
       json: `${siteUrl}/feed.json`,
-      rss: `${siteUrl}/feed.xml`
+      rss: `${siteUrl}/feed.xml`,
     },
     author: {
       name: "CroCoder, Inc.",
@@ -34,12 +34,9 @@ const createFeed = (pages, type = "rss") => {
   const blogs = blogKeys.map((key) => pages[key]);
 
   blogs.forEach((blog) => {
-
     const { Component } = blog;
 
     const blogContent = ReactDOMServer.renderToString(<Component />);
-
-    console.log(blogContent);
 
     const blogAuthor = authors.authors.find(
       (author) => author.id === blog?.meta?.author
@@ -69,16 +66,24 @@ const createFeed = (pages, type = "rss") => {
           link: blogContributor?.linkedin,
         },
       ],
+      date: new Date(blog.meta?.date),
+      published: new Date(blog?.meta?.date),
+      video: blog?.meta?.video,
       image: "", //TODO: Image reference?
+      category: [
+        {
+          name: blog.meta?.category,
+        },
+      ],
     });
   });
 
   switch (type) {
     case "json":
-      return [feed.json1(), 'application/feed+json'];
+      return [feed.json1(), "application/feed+json"];
     case "rss":
     default:
-      return [feed.rss2(), 'application/rss+xml'];
+      return [feed.rss2(), "application/rss+xml"];
   }
 };
 
