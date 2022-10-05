fc401a10d8d8d5e38fc7a1e2bc6ce1bbd26730b7
stefanskoricdev <stephanskoric@gmail.com>
2022-10-05 15:56:06 +0200

    feat: add defer hydration

--- src/client.jsx
+++ src/client.jsx
@@ -4,20 +4,72 @@ import React from 'react';
 import Example from './components/Example';
 import OurClients from './components/OurClients';
 import ContactUs from './components/ContactUs';
+import Card from './components/OurClients/Card';
 
 const components = {
   Example,
   OurClients,
   ContactUs,
+  Card,
+};
+
+const hydrate = (Component, root, props) => {
+  if (!Component) return;
+  ReactDOM.hydrateRoot(root, <Component {...JSON.parse(props)} />);
+};
+
+const initOnDelay = (callback, delay = 300) => {
+  setTimeout(callback, delay);
+};
+
+const initOnIdle = (callback, timeOut = 500) => {
+  if ('requestIdleCallback' in window) {
+    window.requestIdleCallback(callback, { timeout: timeOut });
+  } else {
+    //If rIC is not supported fallover to delay hydration for 200ms
+    initOnDelay(callback, 200);
+  }
+};
+
+const initOnVisible = (callback, element) => {
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(([el]) => {
+      if (!el.isIntersecting || !el.intersectionRatio > 0) return;
+      observer.disconnect();
+      callback();
+    });
+    observer.observe(element);
+  } else {
+    //If IO is not supported fallover to move callback invocation to end of call stack
+    initOnDelay(callback, 0);
+  }
 };
 
 const islands = document.querySelectorAll('[data-name]');
 
 islands.forEach((island) => {
-  console.log(island.dataset);
-  const root = document.querySelector(`[data-name=${island.dataset.name}]`);
-  const props = root.dataset.props;
+  const element = document.querySelector(`[data-name=${island.dataset.name}]`);
+  const props = element.dataset.props;
   const Component = components[island.dataset.name];
-  if (!Component) return;
-  ReactDOM.hydrateRoot(island, <Component {...JSON.parse(props)} />);
+  const deferUntil = element.dataset.deferuntil;
+  switch (deferUntil) {
+    case 'idle':
+      initOnIdle(() => {
+        hydrate(Component, island, props);
+      });
+      break;
+    case 'delay':
+      initOnDelay(() => {
+        hydrate(Component, island, props);
+      });
+      break;
+    case 'visible':
+      initOnVisible(() => {
+        hydrate(Component, island, props);
+      }, element);
+      break;
+    default:
+      hydrate(Component, island, props);
+      break;
+  }
 });
--- src/components/Island/index.jsx
+++ src/components/Island/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Island = ({ children }) => {
+const Island = ({ children, deferUntil }) => {
   if (import.meta.env.SSR) {
     const props = {
       'data-props': JSON.stringify(children.props),
       'data-name': children.type.name,
+      'data-deferuntil': deferUntil || null,
     };
 
     return <island {...props}>{children}</island>;
