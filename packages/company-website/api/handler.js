fc5c0d077fe81b003c74c250dcac11137d64c0e3
David Abram <david@crocoder.dev>
2022-10-26 20:11:15 +0200

    chore: console.log request

--- packages/company-website/api/handler.js
+++ packages/company-website/api/handler.js
@@ -1,6 +1,7 @@
 export default function handler(request, response) {
   // api/[name].ts -> /api/lee
   // req.query.name -> "lee"
+  console.log(request);
   const { name } = request.query;
   return response.end(`Hello ${name}!`);
 }
