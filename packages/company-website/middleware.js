81038f91db121fa38f0124dd07128d3e35820fba
David Abram <david@crocoder.dev>
2022-11-29 10:45:11 +0100

    feat: block stuff

---
+++ packages/company-website/middleware.js
@@ -0,0 +1,24 @@
+import { geolocation } from '@vercel/edge';
+
+const BLOCKED_COUNTRY = 'HR';
+
+export const config = {
+  // Only run the middleware on the home route
+  matcher: '/',
+};
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+
+  const { country } = geolocation(request);
+
+  console.log(country);
+
+  if (country === BLOCKED_COUNTRY) {
+    url.pathname = '/blocked';
+    return Response.redirect(url);
+  }
+
+  // Return a new redirect response
+  return Response.redirect(request.url);
+}
