885f6b51ca5fed32274cfdda9cb439c075c1e8a2
David Abram <david@crocoder.dev>
2022-11-29 11:02:38 +0100

    feat: removed matcher

--- packages/company-website/middleware.js
+++ packages/company-website/middleware.js
@@ -2,10 +2,10 @@ import { geolocation } from '@vercel/edge';
 
 const BLOCKED_COUNTRY = 'HR';
 
-export const config = {
+/* export const config = {
   // Only run the middleware on the home route
   matcher: '^(?!/blocked).*$',
-};
+}; */
 
 export default function middleware(request) {
   const url = new URL(request.url);
