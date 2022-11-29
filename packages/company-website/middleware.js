67de2501687e3c60981d36fb08bc3a4ddbb4017b
David Abram <david@crocoder.dev>
2022-11-29 11:00:56 +0100

    fix: matcher

--- packages/company-website/middleware.js
+++ packages/company-website/middleware.js
@@ -4,7 +4,7 @@ const BLOCKED_COUNTRY = 'HR';
 
 export const config = {
   // Only run the middleware on the home route
-  matcher: '/',
+  matcher: '^(?!/blocked).*$',
 };
 
 export default function middleware(request) {
