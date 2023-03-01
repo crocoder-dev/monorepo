36abccee2cbd9f18b5e8018e8a558bd3d6e957e9
Luka <luka.sesar99@gmail.com>
2023-03-01 20:13:16 +0100

    Fix: installed article-extractor to news app

--- apps/news/app/api/post/route.ts
+++ apps/news/app/api/post/route.ts
@@ -1,11 +1,11 @@
 import { extract } from '@extractus/article-extractor'
 
 export async function GET(request: Request) {
-  console.log(request.body);
+  console.log(request);
   const input = 'https://www.cnbc.com/2022/09/21/what-another-major-rate-hike-by-the-federal-reserve-means-to-you.html';
   const options = {descriptionLengthThreshold: 1000, wordsPerMinute: 150, contentLengthThreshold: 200, descriptionTruncateLen: 1500};
 
   const article = await extract(input, options);
 
-  return Response.json(article)
+  return Response.json({article});
 }
