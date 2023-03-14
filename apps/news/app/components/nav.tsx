a5e303140b67e6616f6d6b7c027aa35ef3ef3b6e
Luka <luka.sesar99@gmail.com>
2023-03-14 17:54:54 +0100

    Update: post-item new date format, removed options button in navigation

--- apps/news/app/components/nav.tsx
+++ apps/news/app/components/nav.tsx
@@ -5,7 +5,7 @@ export default function Nav() {
     <div className="fixed flex justify-center w-full px-4 bg-blue-200 z-50">
       <nav className="flex justify-between items-center w-full max-w-screen-2xl">
         <Link className="inline-block py-3 text-2xl font-bold leading-none text-sky-800" href={'/'}>NEWS</Link>
-        <img className="w-8 h-8 text-sky-800 cursor-pointer" src="/media/nav-options.svg" alt="" />
+        {/* <img className="w-8 h-8 text-sky-800 cursor-pointer" src="/media/nav-options.svg" alt="" /> */}
       </nav>
     </div>
   );
--- apps/news/app/components/post-item.tsx
+++ apps/news/app/components/post-item.tsx
@@ -11,11 +11,12 @@ export default function PostItem({post, setOpen, open}: {post:Post, setOpen: any
     publishedAt,
   } = post;
 
-  const date = publishedAt ? new Date(publishedAt) : null;
 
-  const [month, day, year] = [date ? date.getMonth() + 1 : null, date?.getDate(), date?.getFullYear()];
+  const date = publishedAt ? new Intl.DateTimeFormat('en-US', {month: 'long', day: 'numeric', year: 'numeric'}).format(new Date(publishedAt)) : null;
 
-  const categoryDate = [`${category}`, `${month}/${day}/${year}`].filter(Boolean).join(' | ');
+  // const date = publishedAt ? new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit'}).format(new Date(publishedAt)) : null;
+
+  const categoryDate = [`${category}`, `${date}`].filter(Boolean).join(' | ');
 
   return (
     <li className="flex justify-between space-x-3 relative bg-white hover:bg-gray-50 border-b-2 border-blue-200">
@@ -24,13 +25,13 @@ export default function PostItem({post, setOpen, open}: {post:Post, setOpen: any
           <h1 className="text-2xl font-bold text-sky-800 select-none">{title}</h1>
           <p className="text-lg text-gray-500 select-none">{categoryDate}</p>
         </div>
-        <div className={`px-4 transition-all ease-in-out duration-200 ${open === id ? 'visible' : 'invisible'}`}>
-          {img ? <div className="flex justify-center m-0 pt-3 w-full"><img className="max-h-500px" src={img} alt="post image" /></div> : null}
+        <div className={`px-4 transition-all ease-in-out duration-200 ${open === id ? 'block' : 'hidden'}`}>
+          {img ? <div className="flex justify-center m-0 pt-3 w-full"><img className="max-h-[500px]" src={img} alt="post image" /></div> : null}
           {author ? <div>{`Author: ${author}`}</div> : null}
           {summary.split('\n\n').map(summary=> {
             return <p key={summary} className="py-4 flex flex-col gap-4">{summary}</p>
           })}
-          <a className="inline-block mb-2 text-sky-300 hover:text-sky-500" href={url}>{url}</a>
+          <a target="_blank" className="inline-block mb-2 text-sky-300 hover:text-sky-500" href={url}>{url}</a>
         </div>
       </div>
     </li>
