aea64762baea66a9e3fa5f6d44fe3651aad57414
Luka <luka.sesar99@gmail.com>
2023-03-22 17:00:50 +0100

    Feat/new api version (#58)
    * Update: added emoji to summary and changed some text
    
    * Update: custom route handler now fetches emoji based on title of post
    
    * Update changed summary from 3 paragraphs to 2, post-item small css changes

--- apps/news/app/components/post-item.tsx
+++ apps/news/app/components/post-item.tsx
@@ -8,8 +8,9 @@ export default function PostItem({post}: {post:Post}) {
     summary,
     // img,
     url,
-    // publishedAt,
-    insight
+    publishedAt,
+    insight,
+    emoji
   } = post;
 
 
@@ -21,18 +22,18 @@ export default function PostItem({post}: {post:Post}) {
     <li className="flex justify-between space-x-3 relative">
       <div className="w-full flex-1">
         <div className="block focus:outline-none scroll-mt-16 px-4 py-4">
-          <h1 className="text-2xl font-bold text-sky-800 underline underline-offset-8">{title}</h1>
+          <h1 className="text-2xl font-bold text-sky-800">{emoji} <span className="underline underline-offset-8">{title}</span></h1>
           {/* <p className="text-lg text-gray-500 select-none">{categoryDate}</p> */}
         </div>
         <div className="px-4">
           {/* {img ? <div className="flex justify-center m-0 pt-3 w-full"><img className="max-h-[500px]" src={img} alt="post image" /></div> : null} */}
           {/* {author ? <div>{`Author: ${author}`}</div> : null} */}
-          <span className="font-bold">Summary: </span>
+          <span className="font-bold">TL;DR: </span>
           {summary.split('\n\n').map((summary, i)=> {
-            return <p key={id + '-' + i} className="py-4 flex flex-col gap-4">{summary}</p>
+            return <p key={id + '-' + i} className="py-4 flex flex-col gap-4 text-justify">{summary}</p>
           })}
-          {insight ? <p className="py-4 flex flex-col gap-4"><span className="font-bold">Insight: </span>{insight}</p> : null}
-          <a target="_blank" rel="noopener noreferrer" className="inline-block mb-2 text-sky-300 underline underline-offset-4 hover:text-sky-500" href={url}>You can read the full articel here...</a>
+          {insight ? <p className="py-4 flex flex-col gap-4 text-justify"><span className="font-bold">Insight: </span>{insight}</p> : null}
+          <span className="px-1.5 py-0.5 bg-blue-300 rounded">&#10145;</span>&nbsp;<a target="_blank" rel="noopener noreferrer" className="inline-block mb-2 text-sky-500 underline underline-offset-4 hover:text-sky-300" href={url}>You can read the full article here...</a>
         </div>
       </div>
     </li>
@@ -46,7 +47,8 @@ export type Post = {
   category?: string,
   publishedAt?: string,
   summary: string,
-  insight: string,
+  insight?: string,
+  emoji?: string,
   img?: string,
   url: string
 }
--- apps/news/app/components/posts.tsx
+++ apps/news/app/components/posts.tsx
@@ -4,7 +4,7 @@ export default function Posts({posts}: {posts:Post[]}) {
 
   return (
     <div className="flex justify-center items-center flex-col">
-      <ul className="flex-center w-full px-4 max-w-5xl">
+      <ul className="flex-center w-full px-4 max-w-3xl">
         {
           posts.map((post) => {
             return <PostItem key={post.id} post={post}></PostItem>
