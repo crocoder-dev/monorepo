efc931f58615a66bf4252e0e1ee620e23fd117ea
David Abram <david@crocoder.dev>
2023-04-03 23:38:08 +0200

    feat: random upgrades

--- apps/news/app/api/edition/route.ts
+++ apps/news/app/api/edition/route.ts
@@ -1,18 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getDB } from '@crocoder-dev/db';
 import { editions } from '@crocoder-dev/db/schema';
+import { z } from 'zod';
+import { headers } from 'next/headers';
 
+const inputSchema = z.object({
+  title: z.string(),
+  date: z.number(),
+  slug: z.string(),
+});
 
 export async function POST(request: NextRequest) {
+  const headersList = headers();
+  if( headersList.get('Authorization') !== process.env.API_KEY) {
+    return NextResponse.json({ success: false });
+  }
   const response = new Response(request.body);
-  const { id, title, date } = await response.json();
+  const params = await response.json();
+
+
+  const { title, date, slug } = inputSchema.parse(params);
 
   const db = await getDB();
 
   await db.insert(editions).values({
     title,
-    id,
-    date: new Date(date)
+    date: new Date(date),
+    slug,
   });
 
   return NextResponse.json({ success: true });
