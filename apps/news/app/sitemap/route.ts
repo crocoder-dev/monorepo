adedd27dacebeca12558d004e038553a6d10ba54
David Abram <david@crocoder.dev>
2023-07-03 16:49:38 +0200

    fixed sitemap

--- apps/news/app/sitemap/route.ts
+++ apps/news/app/sitemap/route.ts
@@ -6,25 +6,37 @@ import { getDB } from '@crocoder-dev/db';
 import { Readable } from 'stream';
 
 export async function GET() {
-    const db = getDB();
-  
-    const allEditions = await db.select().from(editions);
+  const db = getDB();
 
-    const links = allEditions.map(e => {
-        return {
-            url: `${siteConfig.siteUrl}${e.slug}`,
-            changefreq: 'monthly',
-            priority: 1.0,
-            lastmod: e.updatedAt
-        }
-    })
+  const allEditions = await db.select().from(editions);
 
-    const stream = new SitemapStream({
-        hostname: siteConfig.siteUrl,
-        lastmodDateOnly: true
-    });
+  const links = allEditions.map(e => {
+    return {
+      url: `${siteConfig.siteUrl}${e.slug}`,
+      changefreq: 'monthly',
+      priority: 1.0,
+      lastmod: e.updatedAt
+    }
+  });
 
-    const promise = await streamToPromise(Readable.from(links).pipe(stream));
+  const today = new Date();
+  today.setDate(today.getDate() - (today.getDay() + 5) % 7)
 
-    return new NextResponse(promise.toString(), { headers: { 'Content-Type': 'application/xml' } });
+  links.push({
+    url: `${siteConfig.siteUrl}`,
+    changefreq: 'weekly',
+    priority: 1.0,
+    lastmod: today,
+  });
+
+  links.reverse();
+
+  const stream = new SitemapStream({
+    hostname: siteConfig.siteUrl,
+    lastmodDateOnly: true
+  });
+
+  const promise = await streamToPromise(Readable.from(links).pipe(stream));
+
+  return new NextResponse(promise.toString(), { headers: { 'Content-Type': 'application/xml' } });
 }
